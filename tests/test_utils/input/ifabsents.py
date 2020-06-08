1a4d748e2a1fa73340f42c1277a3ad19cab07cb9
cmungall <cjm@berkeleybop.org>
2020-06-07 20:10:49 -0700

    Regenerated expected outputs, see #140

--- tests/test_utils/input/ifabsents.py
+++ tests/test_utils/input/ifabsents.py
@@ -20,7 +20,7 @@ else:
 from biolinkml.utils.formatutils import camelcase, underscore, sfx
 from rdflib import Namespace, URIRef
 from biolinkml.utils.curienamespace import CurieNamespace
-from includes.types import String
+from biolinkml.utils.metamodelcore import Bool, ElementIdentifier, NCName, NodeIdentifier, URI, URIorCURIE, XSDDate, XSDDateTime, XSDTime
 
 metamodel_version = "1.4.3"
 
@@ -28,12 +28,117 @@ metamodel_version = "1.4.3"
 dataclasses._init_fn = dataclasses_init_fn_with_kwargs
 
 # Namespaces
+SHEX = CurieNamespace('shex', 'http://www.w3.org/ns/shex#')
 SKOS = CurieNamespace('skos', 'http://www.w3.org/2004/02/skos/core#')
 TEST = CurieNamespace('test', 'http://example.org/test/')
+XSD = CurieNamespace('xsd', 'http://www.w3.org/2001/XMLSchema#')
 DEFAULT_ = TEST
 
 
 # Types
+class String(str):
+    """ A character string """
+    type_class_uri = XSD.string
+    type_class_curie = "xsd:string"
+    type_name = "string"
+    type_model_uri = TEST.String
+
+
+class Integer(int):
+    """ An integer """
+    type_class_uri = XSD.integer
+    type_class_curie = "xsd:integer"
+    type_name = "integer"
+    type_model_uri = TEST.Integer
+
+
+class Boolean(Bool):
+    """ A binary (true or false) value """
+    type_class_uri = XSD.boolean
+    type_class_curie = "xsd:boolean"
+    type_name = "boolean"
+    type_model_uri = TEST.Boolean
+
+
+class Float(float):
+    """ A real number that conforms to the xsd:float specification """
+    type_class_uri = XSD.float
+    type_class_curie = "xsd:float"
+    type_name = "float"
+    type_model_uri = TEST.Float
+
+
+class Double(float):
+    """ A real number that conforms to the xsd:double specification """
+    type_class_uri = XSD.double
+    type_class_curie = "xsd:double"
+    type_name = "double"
+    type_model_uri = TEST.Double
+
+
+class Time(XSDTime):
+    """ A time object represents a (local) time of day, independent of any particular day """
+    type_class_uri = XSD.dateTime
+    type_class_curie = "xsd:dateTime"
+    type_name = "time"
+    type_model_uri = TEST.Time
+
+
+class Date(XSDDate):
+    """ a date (year, month and day) in an idealized calendar """
+    type_class_uri = XSD.date
+    type_class_curie = "xsd:date"
+    type_name = "date"
+    type_model_uri = TEST.Date
+
+
+class Datetime(XSDDateTime):
+    """ The combination of a date and time """
+    type_class_uri = XSD.dateTime
+    type_class_curie = "xsd:dateTime"
+    type_name = "datetime"
+    type_model_uri = TEST.Datetime
+
+
+class Uriorcurie(URIorCURIE):
+    """ a URI or a CURIE """
+    type_class_uri = XSD.anyURI
+    type_class_curie = "xsd:anyURI"
+    type_name = "uriorcurie"
+    type_model_uri = TEST.Uriorcurie
+
+
+class Uri(URI):
+    """ a complete URI """
+    type_class_uri = XSD.anyURI
+    type_class_curie = "xsd:anyURI"
+    type_name = "uri"
+    type_model_uri = TEST.Uri
+
+
+class Ncname(NCName):
+    """ Prefix part of CURIE """
+    type_class_uri = XSD.string
+    type_class_curie = "xsd:string"
+    type_name = "ncname"
+    type_model_uri = TEST.Ncname
+
+
+class Objectidentifier(ElementIdentifier):
+    """ A URI or CURIE that represents an object in the model. """
+    type_class_uri = SHEX.iri
+    type_class_curie = "shex:iri"
+    type_name = "objectidentifier"
+    type_model_uri = TEST.Objectidentifier
+
+
+class Nodeidentifier(NodeIdentifier):
+    """ A URI, CURIE or BNODE that represents a node in a model. """
+    type_class_uri = SHEX.nonLiteral
+    type_class_curie = "shex:nonLiteral"
+    type_name = "nodeidentifier"
+    type_model_uri = TEST.Nodeidentifier
+
 
 # Class references
 
