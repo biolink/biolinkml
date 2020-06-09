0bcb3456be4859e3628d8697915093c33d4cebec
hsolbrig <solbrig@jhu.edu>
2020-06-09 16:22:24 -0500

    Fix Issue #138.  Use relative URL's in context

--- biolinkml/generators/jsonldgen.py
+++ biolinkml/generators/jsonldgen.py
@@ -104,14 +104,7 @@ class JSONLDGenerator(Generator):
         for imp in list(self.loaded.values())[1:]:
             context.append(imp[0] + ".context.jsonld")
 
-        # TODO: For testing purposes we use local contexts.  JSON-LD requires absolute URI's for contexts (double check
-        #  that this is true), so we end up recording local path names in the tests.  All of these must be converted
-        #  to URL's before this can be submitted and used
-        #  Note: This is NOT true -- relative URI's work find.  Fix this next time you have to update local models
-        abs_contexts = ['file://' + os.path.abspath(os.path.join(self.base_dir, c))
-                        if '://' not in c else c for c in context]
-
-        json_obj["@context"] = [abs_contexts[0]] if len(abs_contexts) == 1 and not base_prefix else abs_contexts
+        json_obj["@context"] = [context[0]] if len(context) == 1 and not base_prefix else context
         if base_prefix:
             json_obj["@context"].append({'@base': base_prefix})
         # json_obj["@id"] = self.schema.id
