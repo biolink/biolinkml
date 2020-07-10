bee2fbe4e934ad9c06d759835801c444227629a5
hsolbrig <solbrig@jhu.edu>
2020-07-10 12:27:40 -0500

    Switch test to use slash instead of hash

--- tests/test_issues/test_issue_163.py
+++ tests/test_issues/test_issue_163.py
@@ -32,9 +32,9 @@ class IssueOWLNamespaceTestCase(TestEnvironmentTestCase):
     def test_issue_no_default(self):
         """ Make sure that types are generated as part of the output """
         g = self._test_owl('issue_163b')
-        A = URIRef('http://example.org/sample/example1#A')
+        A = URIRef('http://example.org/sample/example1/A')
         assert (A, RDF.type, OWL.Class) in g
-        NAME = URIRef('http://example.org/sample/example1#name')
+        NAME = URIRef('http://example.org/sample/example1/name')
         assert (NAME, RDF.type, OWL.ObjectProperty) in g
 
     def test_aliases(self):
