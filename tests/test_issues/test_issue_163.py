edb827d7b7f8a84ca3212e8ef56b44fb3db63391
hsolbrig <solbrig@jhu.edu>
2020-07-10 13:09:27 -0500

    Removed warning on slot namespace from owlgen.
    Added default_range to yaml files to reduce testing noise.
    Changed assert statements in test to self.assertIn (asserts can get optimized out...)

--- tests/test_issues/test_issue_163.py
+++ tests/test_issues/test_issue_163.py
@@ -25,17 +25,17 @@ class IssueOWLNamespaceTestCase(TestEnvironmentTestCase):
         """ Make sure that types are generated as part of the output """
         g = self._test_owl('issue_163')
         A = URIRef('http://example.org/A')
-        assert (A, RDF.type, OWL.Class) in g
+        self.assertIn((A, RDF.type, OWL.Class), g)
         NAME = URIRef('http://example.org/name')
-        assert (NAME, RDF.type, OWL.ObjectProperty) in g
+        self.assertIn((NAME, RDF.type, OWL.ObjectProperty), g)
 
     def test_issue_no_default(self):
         """ Make sure that types are generated as part of the output """
         g = self._test_owl('issue_163b')
         A = URIRef('http://example.org/sample/example1/A')
-        assert (A, RDF.type, OWL.Class) in g
+        self.assertIn((A, RDF.type, OWL.Class), g)
         NAME = URIRef('http://example.org/sample/example1/name')
-        assert (NAME, RDF.type, OWL.ObjectProperty) in g
+        self.assertIn((NAME, RDF.type, OWL.ObjectProperty), g)
 
     def test_aliases(self):
         """ Make sure aliases work """
