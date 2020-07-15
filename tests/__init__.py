ae4fa690e2f046d8b51f9cb9bca68224be239651
jiaola <djiao@jhu.edu>
2020-07-14 23:57:56 -0400

    Issue #194. Use a local config file for test settings.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,39 +1,45 @@
 import logging
+import configparser
 # Global testing control variables
-
-
 from tests.utils.test_environment import MismatchAction
 
+# Don't change this file. Use test_config.ini for local settings.
+
+config = configparser.ConfigParser()
+config.read('test_config.ini')
+if 'test.settings' not in config.sections():
+    config['test.settings'] = {}  # initialize a blank setting if file doesn't exist
+test_settings = config['test.settings']
+
 # There are lots of warnings emitted by the generators. Default logging level
-DEFAULT_LOG_LEVEL = logging.ERROR
-DEFAULT_LOG_LEVEL_TEXT = 'ERROR'
+DEFAULT_LOG_LEVEL = eval(test_settings.get('DEFAULT_LOG_LEVEL', 'logging.ERROR'))
+DEFAULT_LOG_LEVEL_TEXT = test_settings.get('DEFAULT_LOG_LEVEL_TEXT', 'ERROR')
 
 # Parts of the ShEx validation still need some performance optimization
-SKIP_SHEX_VALIDATION = True
-SKIP_SHEX_VALIDATION_REASON = "tests/__init__.py ShEx validation skipped: SKIP_SHEX_VALIDATION is True"
+SKIP_SHEX_VALIDATION = test_settings.getboolean('SKIP_SHEX_VALIDATION', True)
+SKIP_SHEX_VALIDATION_REASON = test_settings.get('SKIP_SHEX_VALIDATION_REASON', 'tests/__init__.py ShEx validation skipped: SKIP_SHEX_VALIDATION is True')
 
 # Sometimes it is convenient to bypass graphviz testing output
 # NOTE: This will remain disabled until it is determined whether GraphViz output is still needed
-SKIP_GRAPHVIZ_VALIDATION = True
-SKIP_GRAPHVIZ_VALIDATION_REASON = "tests/__init__.py GraphViz generation skipped SKIP_GRAPHVIZ_VALIDATION is True"
+SKIP_GRAPHVIZ_VALIDATION = test_settings.getboolean('SKIP_GRAPHVIZ_VALIDATION', True)
+SKIP_GRAPHVIZ_VALIDATION_REASON = test_settings.get('SKIP_GRAPHVIZ_VALIDATION_REASON', 'tests/__init__.py GraphViz generation skipped SKIP_GRAPHVIZ_VALIDATION is True')
 
 # Same for markdown
-SKIP_MARKDOWN_VALIDATION = False
-SKIP_MARKDOWN_VALIDATION_REASON = "tests/__init__.py Markdown generation SKIP_MARKDOWN_VALIDATION is True"
+SKIP_MARKDOWN_VALIDATION = test_settings.getboolean('SKIP_MARKDOWN_VALIDATION', False)
+SKIP_MARKDOWN_VALIDATION_REASON = test_settings.get('SKIP_MARKDOWN_VALIDATION_REASON', 'tests/__init__.py Markdown generation SKIP_MARKDOWN_VALIDATION is True')
 
 # Skip RDF comparison, as it takes a lot of time
-SKIP_RDF_COMPARE = True
-SKIP_RDF_COMPARE_REASON = "tests/__init__.py RDF output not checked SKIP_RDF_COMPARE is True"
+SKIP_RDF_COMPARE = test_settings.getboolean('SKIP_RDF_COMPARE', True)
+SKIP_RDF_COMPARE_REASON = test_settings.get('SKIP_RDF_COMPARE_REASON', 'tests/__init__.py RDF output not checked SKIP_RDF_COMPARE is True')
 
 # Skip Rewrite rules tests -- these only get re-tested when we change the w3id.org server
-SKIP_REWRITE_RULES = True
-SKIP_REWRITE_RULES_REASON = "tests/__init__.py SKIP_REWRITE_RULES is True"
+SKIP_REWRITE_RULES = test_settings.getboolean('SKIP_REWRITE_RULES', True)
+SKIP_REWRITE_RULES_REASON = test_settings.get('SKIP_REWRITE_RULES_REASON', 'tests/__init__.py SKIP_REWRITE_RULES is True')
 
 # Action on mismatch.  One of 'Ignore', 'Report' or 'Fail'
 #  If 'Fail', the expected file will be saved in the appropriate temp directory
 #  NOTE: Before setting this back to Report or Ignore, you need to run cleartemp.sh in this directory
-DEFAULT_MISMATCH_ACTION = MismatchAction.Report
-
+DEFAULT_MISMATCH_ACTION = eval(test_settings.get('DEFAULT_MISMATCH_ACTION', 'MismatchAction.Report'))
 
 # Exception for use in script testing.  Global to prevent redefinition
 class CLIExitException(Exception):
