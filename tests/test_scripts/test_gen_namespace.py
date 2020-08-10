a1af649a0dbb6618f874e6b92b4b6537421ad57e
hsolbrig <solbrig@jhu.edu>
2020-08-10 15:45:42 -0500

    Additional input for issue #101
    Make the namespace generator an official CLI entry
    Change the name of the test_biolink_model output to namespaces.py
    Add test_gen_namespace.py to tests/test_scripts
    Add the python comparator to the gen_python so the output gets tested for compilability
    add gen-namespaces to the console scripts

---
+++ tests/test_scripts/test_gen_namespace.py
@@ -0,0 +1,32 @@
+import unittest
+from types import ModuleType
+
+import click
+
+
+from biolinkml.generators import namespacegen
+from tests.test_scripts.environment import env
+from tests.utils.clicktestcase import ClickTestCase
+from tests.utils.filters import metadata_filter
+from tests.utils.python_comparator import compare_python
+
+
+class GenNamespaceTestCase(ClickTestCase):
+    testdir = "gennamespace"
+    click_ep = namespacegen.cli
+    prog_name = "gen-namespace"
+    env = env
+
+
+    def test_help(self):
+        self.do_test("--help", 'help')
+
+    def test_meta(self):
+        self.maxDiff = None
+        self.do_test([], 'meta_namespaces.py', filtr=metadata_filter, comparator=compare_python)
+        self.do_test('-f py', 'meta_namespaces.py', filtr=metadata_filter, comparator=compare_python)
+        self.do_test('-f xsv', 'meta_error', expected_error=click.exceptions.BadParameter)
+
+
+if __name__ == '__main__':
+    unittest.main()
