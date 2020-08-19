09fcc308ba438e7f38ed67d01b5f18a49ad881da
GitHub Action <action@github.com>
2020-08-19 21:03:40 +0000

    Automated adding outputs from tests

--- tests/test_issues/output/issue_ttcm_1.py
+++ tests/test_issues/output/issue_ttcm_1.py
@@ -1,5 +1,5 @@
 # Auto generated from resourcedescription.yaml by pythongen.py version: 0.4.0
-# Generation date: 2020-08-19 13:24
+# Generation date: 2020-08-19 21:02
 # Schema: resourcedescription
 #
 # id: https://hotecosystem.org/tccm/resourcedescription
