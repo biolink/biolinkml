08558cc1dca36bc39684af6b69ca9836bd0dc109
hsolbrig <solbrig@jhu.edu>
2020-08-25 12:42:56 -0500

    Partial fix for issue #258.  Set model version to 1.5.8, next PyPi addition

--- biolinkml/meta.py
+++ biolinkml/meta.py
@@ -1,5 +1,5 @@
-# Auto generated from .yaml by pythongen.py version:
-# Generation date:
+# Auto generated from meta.yaml by pythongen.py version: 0.4.0
+# Generation date: 2020-08-25 16:38
 # Schema: metamodel
 #
 # id: https://w3id.org/biolink/biolinkml/meta
