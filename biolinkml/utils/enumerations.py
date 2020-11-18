cefe8cf951e3457d18b023dbeb55e992246119f2
hsolbrig <solbrig@jhu.edu>
2020-11-18 14:39:57 -0600

    Add an automated update of the notebooks directory

--- biolinkml/utils/enumerations.py
+++ biolinkml/utils/enumerations.py
@@ -2,6 +2,11 @@ from biolinkml.meta import EnumDefinition
 
 
 class EnumerationHandler(EnumDefinition):
+    """
+    This is the abstract API for Enumeration processing.  The default handler is intended to handle the case where the
+    enumerations have explicit permissible values and, as such, it is not necessary to refer to a code_set to determine
+    the permissible values or their mappings.
+    """
 
     def __contains__(self, item):
         return item in self.permissible_values
