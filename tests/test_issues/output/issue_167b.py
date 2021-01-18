fa3dc20979be14466fca1c15ba53ad325eb5bd4e
GitHub Action <action@github.com>
2021-01-18 21:41:46 +0000

    Automated adding outputs from tests

--- tests/test_issues/output/issue_167b.py
+++ tests/test_issues/output/issue_167b.py
@@ -1,5 +1,5 @@
 # Auto generated from issue_167b.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-01-18 20:41
+# Generation date: 2021-01-18 21:40
 # Schema: annotations_test
 #
 # id: http://example.org/tests/issue167b
